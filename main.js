d612b1663d54fa4e81904c6ef67feebada1e49da
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-07-03 20:34:22 +0200

    get item price & name on click

--- main.js
+++ main.js
@@ -5,5 +5,42 @@ for (const adCartBtn of adCartBtns) {
 }
 
 // -- functions
+// function getParentElem(elem) {
+//     console.log(elem)
+//     parentElem = elem.parentElement
+//     console.log(parentElem)    
+
+//     return parentElem
+// }
+
+function getProdName(elem) {
+    console.log(elem)
+    const parentElem = elem.parentElement
+    console.log(parentElem)
+    const prodName = parentElem.querySelector('.product-name').innerHTML
+
+    return prodName
+}
+
+function getProdPrice(elem) {
+    console.log(elem)
+    const parentElem = elem.parentElement
+    console.log(parentElem)
+    const prodPrice = parentElem.querySelector('.product-price-number').innerHTML
+
+    return prodPrice
+}
+
+function add2Cart(elem) {
+    console.log(elem)
+
+    const prodName = getProdName(elem)
+    console.log(prodName)
+
+    const prodPrice = getProdPrice(elem)
+    console.log(prodPrice)
+
+    return {prodName, prodPrice}
+}
 
 // -- script
